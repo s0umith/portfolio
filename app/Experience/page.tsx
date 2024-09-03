5382d8dcd2b887fa953a1e19df1f066d0604a011
Teja <103510023+NaveentejaB@users.noreply.github.com>
2024-09-03 22:44:54 +0530

    test 19

--- app/Experience/page.tsx
+++ app/Experience/page.tsx
@@ -7,7 +7,7 @@ export default function Page(){
             <div className="w-full h-2/5 flex justify-center items-center ">
                 <h2 className="gradient-text text-6xl font-semibold ">EXPERIENCE</h2>
             </div>
-            <div className="w-full h-full flex flex-col justify-start items-center gap-20">
+            <div className="w-full h-full flex flex-col justify-start items-center gap-20 ">
                 <ExperienceCard/>
                 <ExperienceCard/>
                 <ExperienceCard/>
