a057bfe9308100af487103ea936412f6ea611d95
Teja <103510023+NaveentejaB@users.noreply.github.com>
2024-10-10 06:52:53 +0530

    bug fixed 1

--- components/small-components/footer.tsx
+++ components/small-components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer(){
         <footer className="pt-20 w-full flex flex-col text-white items-center gap-2 py-4">
             <div className="flex gap-2 md:gap-4">
                 { socialMedia.map((data,i)=>(
-                    <Link href={data.url}  rel="noopener noreferrer" target="_blank">
+                    <Link href={data.url} key={i}  rel="noopener noreferrer" target="_blank">
                         <div className="p-1 rounded-full cursor-pointer">
                             <Image className=" w-5 h-5  md:w-6 md:h-6  flex" src={data.svgPath} alt="img" width={8} height={8}/>
                         </div>
